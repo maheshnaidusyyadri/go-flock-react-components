b5fc15bbb3f599abf6af8a425e0ae1f6f3f6021f
syam <syamalj@innvectra.com>
2024-10-05 02:14:48 +0530

    Event List updated

--- src/components/EventList/EventListPresenter.tsx
+++ src/components/EventList/EventListPresenter.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import {
-  IonCard,
+import "./EventListPresenter.scss";
+import { 
   IonCardHeader,
   IonCardTitle,
-  IonCardContent,
-  IonAvatar,
+  IonCardContent, 
   IonList,
+  IonContent, 
 } from "@ionic/react";
 import { EventListProps } from "@goflock/types/src/index";
 import EventItem from "../Common/Events/EventItem";
+import Header from "../Header/Header";
 
 const EventListPresenter: React.FC<EventListProps> = ({
   profile,
@@ -16,33 +17,40 @@ const EventListPresenter: React.FC<EventListProps> = ({
   openEvent,
 }) => {
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonAvatar slot="start">
-          <img
-            src={profile.pictureUrl}
-            alt="Profile"
-          />
-        </IonAvatar>
-        <IonCardTitle>{profile.prefName}'s Events</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        {events.length === 0 ? (
-          <p>No events to display.</p>
-        ) : (
-          <IonList>
-            {events.map((event) => (
-              <EventItem
-                key={event.id}
-                event={event}
-                onOpen={openEvent}
-                onShowActionSheet={() => {}}
-              />
-            ))}
-          </IonList>
-        )}
-      </IonCardContent>
-    </IonCard>
+    <>
+    <Header
+        
+        //title={event.name}
+        title=""
+        showMenu={true}
+        showContactList={false}         
+      />
+ 
+      <IonContent className="events_sec">
+          <IonCardHeader className="events_head">
+            <IonCardTitle className="events_title">{profile.prefName}'s Events</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className="events_cnt">
+            {events.length === 0 ? (
+              <p>No events to display.</p>
+            ) : (
+              <IonList className="ion-list">
+                {events.map((event) => (
+                  <EventItem
+                    key={event.id}
+                    event={event}
+                    onOpen={openEvent}
+                    onShowActionSheet={() => {}}
+                  />
+                ))}
+              </IonList>
+            )}
+          </IonCardContent>
+        
+         
+       
+    </IonContent>
+    </>
   );
 };
 
