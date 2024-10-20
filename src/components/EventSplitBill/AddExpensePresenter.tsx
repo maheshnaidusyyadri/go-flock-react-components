736ae6782f3bcb3219cb96944b2073542b5e2c85
syam <syamalj@innvectra.com>
2024-10-20 15:05:37 +0530

    buttons updated

--- src/components/EventSplitBill/AddExpensePresenter.tsx
+++ src/components/EventSplitBill/AddExpensePresenter.tsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./EventSplitBillPresenter.scss";
 
 import {
-  IonButton, 
+  IonButton,
   IonLabel,
-  IonText,  
+  IonText,
   IonFooter,
-  IonGrid, 
-  IonImg, 
+  IonGrid,
+  IonImg,
   IonThumbnail,
   IonItem,
   IonCardContent,
@@ -21,33 +21,30 @@ import {
   IonContent,
 } from "@ionic/react";
 import {
- // EventSplitBillProps,
-  EventVisibility, 
+  // EventSplitBillProps,
+  EventVisibility,
 } from "@goflock/types/src/index"; // Adjust the import based on your file structure
 // import { checkmarkCircle, ellipseOutline } from 'ionicons/icons';
-import EqualIcon from "../../images/icons/Equal.svg"; 
-import DollarIcon from "../../images/icons/Dollar.svg"; 
-import PercentIcon from "../../images/icons/Percent.svg"; 
-import ProfileIcon from "../../images/profile.png"; 
+import EqualIcon from "../../images/icons/Equal.svg";
+import DollarIcon from "../../images/icons/Dollar.svg";
+import PercentIcon from "../../images/icons/Percent.svg";
+import ProfileIcon from "../../images/profile.png";
 
-import Header from "../Header/Header"; 
- 
+import Header from "../Header/Header";
 
 interface EventSplitBillProps {
-  members: { 
-    name: string; 
-    phone: string;  
-    expanse: string; 
-    profileImage?: string 
-    className : string
+  members: {
+    name: string;
+    phone: string;
+    expanse: string;
+    profileImage?: string;
+    className: string;
   }[];
-   
 }
 
 const EventBillPresenter: React.FC<EventSplitBillProps> = ({
-  members = [ ],
+  members = [],
 }) => {
-  
   // @ts-ignore
   const [endDate, setEndDate] = useState<Date>(new Date());
   // @ts-ignore
@@ -56,7 +53,6 @@ const EventBillPresenter: React.FC<EventSplitBillProps> = ({
   const [endTime, setEndTime] = useState<string>("12:00 PM");
 
   const [] = useState<EventVisibility>();
-
 
   const [currentStep, setCurrentStep] = useState(1); // Track the current step
   const totalSteps = 3; // Define the total number of steps
@@ -80,230 +76,194 @@ const EventBillPresenter: React.FC<EventSplitBillProps> = ({
       ></div>
     ));
   };
-  
+
   const getStepClass = (step: number) => {
     if (step < currentStep) return "prev";
     if (step === currentStep) return "current";
     if (step > currentStep) return "next";
     return "";
   };
-   
 
   function setEventName(_arg0: string): void {
     throw new Error("Function not implemented.");
   }
 
   const [isOpen, setIsOpen] = useState(false);
-  
-  return (
-    <>  
-      <Header         
-        title='Add expense'
-        showMenu={false}
-        showContactList={false} 
-        showProfile = {true} 
-      />
-      <IonGrid
-        className="pageCnt create_event"        
-      >
-        <IonList className="stepper-container">{renderSteps()}</IonList>
-          <IonGrid className="stepper-content"> 
-          <IonGrid className={`step-content ${getStepClass(1)}`}>
-              <IonGrid className="form-container">
-                <IonCardContent className="pad0">
-                  <IonList className="form-group">
-                    <IonInput
-                      value=''
-                      label="Expense details*"
-                      labelPlacement="stacked"
-                      placeholder="Enter bill name"
-                      onIonChange={(e) => setEventName(e.detail.value!)}
-                    />
-                  </IonList>
-                  <IonList className="form-group">
-                    <IonInput
-                      value=''
-                      label="Total Amount*"
-                      labelPlacement="stacked"
-                      placeholder="Enter amount"
-                      onIonChange={(e) => setEventName(e.detail.value!)}
-                    />
-                  </IonList>
-                  <IonList className="form-group" onClick={() => setIsOpen(true)}>
-                    <IonInput
-                      value=''
-                      label="Paid by*"
-                      labelPlacement="stacked"
-                      placeholder="You"
-                      onIonChange={(e) => setEventName(e.detail.value!)}
-                    />
-                  </IonList>
-                  <IonList className="form-group">
-                    <IonInput
-                      value=''
-                      label="Split among*"
-                      labelPlacement="stacked"
-                      placeholder="Choose members"
-                      onIonChange={(e) => setEventName(e.detail.value!)}
-                      readonly
-                    />
-                  </IonList>
-                    
-                </IonCardContent>
-              </IonGrid>
-            </IonGrid>
-            <IonGrid className={`step-content ${getStepClass(2)}`}>
-            <IonTabs className="expense_tabs">
-                <IonTabBar slot="top">
-                  <IonTabButton tab="home">
-                    <IonImg 
-                     src={EqualIcon} />
-                  </IonTabButton>
-                  <IonTabButton tab="radio">
-                    <IonImg 
-                     src={DollarIcon} />
-                  </IonTabButton>
-                  <IonTabButton tab="library">
-                    <IonImg 
-                     src={PercentIcon} />
-                  </IonTabButton> 
-                </IonTabBar>
 
-                <IonTab tab="home">
-                  <div id="home-page">
-                    <IonList className="list_wrap"> 
-                      {members.map((member, index) => (
-                        <IonItem
-                          key={index}
-                          className="user_item" 
-                        >
-                          <IonThumbnail
-                            slot="start"
-                            className="dp"
-                          >
-                            <IonImg
-                              src={ProfileIcon}
-                              alt={`${member.name}'s profile`}
-                            /> 
-                          </IonThumbnail>
-                          <IonLabel className="user_name">
-                            {member.name}
-                            {member.phone}
-                          </IonLabel>
-                          <IonText class="amout">
-                            {member.expanse}
-                          </IonText>
-                        </IonItem>
-                      ))}
-                    </IonList> 
-                  </div>
-                </IonTab>
-                <IonTab tab="radio">
-                  <div id="radio-page">
-                    <IonList className="list_wrap"> 
-                      {members.map((member, index) => (
-                        <IonItem
-                          key={index}
-                          className="user_item" 
-                        >
-                          <IonThumbnail
-                            slot="start"
-                            className="dp"
-                          >
-                            <IonImg
-                              src={ProfileIcon}
-                              alt={`${member.name}'s profile`}
-                            /> 
-                          </IonThumbnail>
-                          <IonLabel className="user_name">
-                            {member.name}
-                            {member.phone}
-                          </IonLabel>
-                          <IonInput
-                                  className="ion_input prefix"
-                                  value=''
-                                  label=""
-                                  labelPlacement="stacked"
-                                  placeholder="0.00"
-                                  type="number"  // Ensures numeric input
-                                  inputmode="decimal"
-                                  onIonChange={(e) => setEventName(e.detail.value!)} 
-                                />
-                        </IonItem>
-                      ))}
-                    </IonList>                     
-                  </div>
-                </IonTab>
-                <IonTab tab="library">
-                  <div id="library-page">
-                    <IonList className="list_wrap"> 
-                      {members.map((member, index) => (
-                        <IonItem
-                          key={index}
-                          className="user_item" 
-                        >
-                          <IonThumbnail
-                            slot="start"
-                            className="dp"
-                          >
-                            <IonImg
-                              src={ProfileIcon}
-                              alt={`${member.name}'s profile`}
-                            /> 
-                          </IonThumbnail>
-                          <IonLabel className="user_name">
-                            {member.name}
-                            {member.phone}
-                          </IonLabel>
-                          <IonInput
-                            class="ion_input safix"
-                            value=''
-                            label=""
-                            labelPlacement="stacked"
-                            placeholder="0"
-                            type="number"  // Ensures numeric input
-                            inputmode="decimal"
-                            onIonChange={(e) => setEventName(e.detail.value!)} 
-                          />
-                        </IonItem>
-                      ))}
-                    </IonList>
-                  </div>
-                </IonTab> 
-              </IonTabs>
+  return (
+    <>
+      <Header
+        title="Add expense"
+        showMenu={false}
+        showContactList={false}
+        showProfile={true}
+      />
+      <IonGrid className="expense_page">
+        <IonList className="stepper-container">{renderSteps()}</IonList>
+        <IonGrid className="stepper-content">
+          <IonGrid className={`step-content ${getStepClass(1)}`}>
+            <IonGrid className="form-container">
+              <IonCardContent className="pad0">
+                <IonList className="form-group">
+                  <IonInput
+                    value=""
+                    label="Expense details*"
+                    labelPlacement="stacked"
+                    placeholder="Enter bill name"
+                    onIonChange={(e) => setEventName(e.detail.value!)}
+                  />
+                </IonList>
+                <IonList className="form-group">
+                  <IonInput
+                    value=""
+                    label="Total Amount*"
+                    labelPlacement="stacked"
+                    placeholder="Enter amount"
+                    onIonChange={(e) => setEventName(e.detail.value!)}
+                  />
+                </IonList>
+                <IonList className="form-group" onClick={() => setIsOpen(true)}>
+                  <IonInput
+                    value=""
+                    label="Paid by*"
+                    labelPlacement="stacked"
+                    placeholder="You"
+                    onIonChange={(e) => setEventName(e.detail.value!)}
+                  />
+                </IonList>
+                <IonList className="form-group">
+                  <IonInput
+                    value=""
+                    label="Split among*"
+                    labelPlacement="stacked"
+                    placeholder="Choose members"
+                    onIonChange={(e) => setEventName(e.detail.value!)}
+                    readonly
+                  />
+                </IonList>
+              </IonCardContent>
             </IonGrid>
-            <IonGrid className={`step-content ${getStepClass(3)}`}>
-            <IonList className="list_wrap"> 
+          </IonGrid>
+          <IonGrid className={`step-content ${getStepClass(2)}`}>
+            <IonTabs className="expense_tabs">
+              <IonTabBar slot="top">
+                <IonTabButton tab="home">
+                  <IonImg src={EqualIcon} />
+                </IonTabButton>
+                <IonTabButton tab="radio">
+                  <IonImg src={DollarIcon} />
+                </IonTabButton>
+                <IonTabButton tab="library">
+                  <IonImg src={PercentIcon} />
+                </IonTabButton>
+              </IonTabBar>
+
+              <IonTab tab="home">
+                <div id="home-page">
+                  <IonList className="list_wrap">
+                    {members.map((member, index) => (
+                      <IonItem key={index} className="user_item">
+                        <IonThumbnail slot="start" className="dp">
+                          <IonImg
+                            src={ProfileIcon}
+                            alt={`${member.name}'s profile`}
+                          />
+                        </IonThumbnail>
+                        <IonLabel className="user_name">
+                          {member.name}
+                          {member.phone}
+                        </IonLabel>
+                        <IonText class="amout">{member.expanse}</IonText>
+                      </IonItem>
+                    ))}
+                  </IonList>
+                </div>
+              </IonTab>
+              <IonTab tab="radio">
+                <div id="radio-page">
+                  <IonList className="list_wrap">
+                    {members.map((member, index) => (
+                      <IonItem key={index} className="user_item">
+                        <IonThumbnail slot="start" className="dp">
+                          <IonImg
+                            src={ProfileIcon}
+                            alt={`${member.name}'s profile`}
+                          />
+                        </IonThumbnail>
+                        <IonLabel className="user_name">
+                          {member.name}
+                          {member.phone}
+                        </IonLabel>
+                        <IonInput
+                          className="ion_input prefix"
+                          value=""
+                          label=""
+                          labelPlacement="stacked"
+                          placeholder="0.00"
+                          type="number" // Ensures numeric input
+                          inputmode="decimal"
+                          onIonChange={(e) => setEventName(e.detail.value!)}
+                        />
+                      </IonItem>
+                    ))}
+                  </IonList>
+                </div>
+              </IonTab>
+              <IonTab tab="library">
+                <div id="library-page">
+                  <IonList className="list_wrap">
+                    {members.map((member, index) => (
+                      <IonItem key={index} className="user_item">
+                        <IonThumbnail slot="start" className="dp">
+                          <IonImg
+                            src={ProfileIcon}
+                            alt={`${member.name}'s profile`}
+                          />
+                        </IonThumbnail>
+                        <IonLabel className="user_name">
+                          {member.name}
+                          {member.phone}
+                        </IonLabel>
+                        <IonInput
+                          class="ion_input safix"
+                          value=""
+                          label=""
+                          labelPlacement="stacked"
+                          placeholder="0"
+                          type="number" // Ensures numeric input
+                          inputmode="decimal"
+                          onIonChange={(e) => setEventName(e.detail.value!)}
+                        />
+                      </IonItem>
+                    ))}
+                  </IonList>
+                </div>
+              </IonTab>
+            </IonTabs>
+          </IonGrid>
+          <IonGrid className={`step-content ${getStepClass(3)}`}>
+            <IonList className="list_wrap">
               {members.map((member, index) => (
-                <IonItem
-                  key={index}
-                  className="user_item" 
-                >
-                  <IonThumbnail
-                    slot="start"
-                    className="dp"
-                  >
+                <IonItem key={index} className="user_item">
+                  <IonThumbnail slot="start" className="dp">
                     <IonImg
                       src={ProfileIcon}
                       alt={`${member.name}'s profile`}
-                    /> 
+                    />
                   </IonThumbnail>
                   <IonLabel className="user_name">
-                     {member.name}
-                     {member.phone}
+                    {member.name}
+                    {member.phone}
                   </IonLabel>
                   <IonText class="amout" className={member.className}>
                     {member.expanse}
                   </IonText>
                 </IonItem>
               ))}
-            </IonList> 
-            </IonGrid>
+            </IonList>
           </IonGrid>
-          {/* <IonFooter className="action_screen_buttons">
-            <IonButton className="primary-btn rounded">Next</IonButton> 
-          </IonFooter> */}
-          <IonFooter className="actions-container">
+        </IonGrid>
+        <IonFooter className="actions-container stickyFooter">
           <IonButton
             className="primary-btn actions"
             onClick={prevStep}
@@ -320,19 +280,19 @@ const EventBillPresenter: React.FC<EventSplitBillProps> = ({
           </IonButton>
           {currentStep === totalSteps && (
             <IonButton
-              className="primary-btn"
-             // onClick={() => handleCreateEvent()}
+              className="primary-btn save"
+              // onClick={() => handleCreateEvent()}
               expand="block"
               //disabled={isCreating || !selectedLocation || !eventName}
             >
-               Save & Back To Home
+              Save & Back To Home
             </IonButton>
           )}
         </IonFooter>
-      </IonGrid> 
+      </IonGrid>
 
       <IonModal isOpen={isOpen}>
-          {/* <IonHeader>
+        {/* <IonHeader>
             <IonToolbar>
               <IonTitle>Modal</IonTitle>
               <IonButtons slot="end">
@@ -340,15 +300,16 @@ const EventBillPresenter: React.FC<EventSplitBillProps> = ({
               </IonButtons>
             </IonToolbar>
           </IonHeader> */}
-          <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
-          <IonContent className="ion-padding">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
-              reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
-              Eaque, dicta.
-            </p>
-          </IonContent>
-        </IonModal>
+        <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
+        <IonContent className="ion-padding">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum
+            quidem recusandae ducimus quos reprehenderit. Veniam, molestias
+            quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
+            Eaque, dicta.
+          </p>
+        </IonContent>
+      </IonModal>
     </>
   );
 };
--- src/components/EventSplitBill/ExpensesPresenter.tsx
+++ src/components/EventSplitBill/ExpensesPresenter.tsx
@@ -1,45 +1,35 @@
 import React, { useState } from "react";
 import "./EventSplitBillPresenter.scss";
 import {
-  IonButton, 
+  IonButton,
   IonLabel,
-  IonText,  
+  IonText,
   IonFooter,
-  IonGrid, 
-  IonImg, 
+  IonGrid,
+  IonImg,
   IonThumbnail,
   IonItem,
   IonList,
 } from "@ionic/react";
-import {
-  //EventSplitBillProps, 
-} from "@goflock/types/src/index"; // Adjust the import based on your file structure
-// import { checkmarkCircle, ellipseOutline } from 'ionicons/icons';
-import "react-datepicker/dist/react-datepicker.css"; 
-import EditIcon from "../../images/icons/EditWhite.svg"; 
-import ProfileIcon from "../../images/profile.png";  
+import //EventSplitBillProps,
+"@goflock/types/src/index"; // Adjust the import based on your file structure
+import EditIcon from "../../images/icons/EditWhite.svg";
+import ProfileIcon from "../../images/profile.png";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
-
 interface EventSplitBillProps {
-  members: { 
-    name: string; 
-    phone: string;  
-    expanse: string; 
-    profileImage?: string 
-    className : string
+  members: {
+    name: string;
+    phone: string;
+    expanse: string;
+    profileImage?: string;
+    className: string;
   }[];
-   
 }
 
-const ExpensesPresenter: React.FC<EventSplitBillProps> = ({ 
-  members = [ ],
- 
-
-}) => {
-   
+const ExpensesPresenter: React.FC<EventSplitBillProps> = ({ members = [] }) => {
   // @ts-ignore
   const [startDate, setStartDate] = useState<Date>(new Date());
   // @ts-ignore
@@ -49,52 +39,38 @@ const ExpensesPresenter: React.FC<EventSplitBillProps> = ({
   // @ts-ignore
   const [endTime, setEndTime] = useState<string>("12:00 PM");
 
-  
- 
-
-   
-
   return (
-    <>  
-      <Header         
-        title='Expenses'
+    <>
+      <Header
+        title="Expenses"
         showMenu={false}
-        showContactList={false} 
-        showProfile = {true} 
+        showContactList={false}
+        showProfile={true}
       />
-      <IonGrid
-        className="pageCnt bill_cnt"        
-      > 
-          <IonList className="list_wrap">  
-              {members.map((member, index) => (
-                <IonItem
-                  key={index}
-                  className="user_item" 
-                > 
-                  <IonThumbnail
-                    slot="start"
-                    className="dp"
-                  >
-                    <IonImg
-                      src={ProfileIcon}
-                      alt={`${member.name}'s profile`}
-                    /> 
-                  </IonThumbnail>
-                  <IonLabel className="user_name">
-                     {member.name}
-                     {member.phone}
-                  </IonLabel>
-                  <IonText class="amout" className={member.className}>
-                    {member.expanse}
-                  </IonText>
-                </IonItem>
-              ))}
-          </IonList>       
+      <IonGrid className="pageCnt bill_cnt">
+        <IonList className="list_wrap">
+          {members.map((member, index) => (
+            <IonItem key={index} className="user_item">
+              <IonThumbnail slot="start" className="dp">
+                <IonImg src={ProfileIcon} alt={`${member.name}'s profile`} />
+              </IonThumbnail>
+              <IonLabel className="user_name">
+                {member.name}
+                {member.phone}
+              </IonLabel>
+              <IonText class="amout" className={member.className}>
+                {member.expanse}
+              </IonText>
+            </IonItem>
+          ))}
+        </IonList>
         <IonFooter className="action_screen_buttons">
-          <IonButton className="primary-btn rounded"><IonImg src={EditIcon} /> Edit Expense</IonButton> 
+          <IonButton className="primary-btn rounded">
+            <IonImg src={EditIcon} /> Edit Expense
+          </IonButton>
         </IonFooter>
       </IonGrid>
-      <Footer eventId='' activeTab={"bills"} />
+      <Footer eventId="" activeTab={"bills"} />
     </>
   );
 };
