33df7107364cc844c3f51f305d842b875058f57c
syam <syamalj@innvectra.com>
2024-11-15 17:39:49 +0530

    Event type icons updated

--- src/components/Common/EventType.tsx
+++ src/components/Common/EventType.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import {
-  IonCol,
-  IonGrid,
-  IonRow,
-  IonContent,
-  IonImg,
-  IonCard,
-} from "@ionic/react";
-import settingsIcon from "../../images/icons/settingsCircle.svg";
-import quickSetupIcon from "../../images/icons/quickSetup.svg";
-import detailSetupIcon from "../../images/icons/detailSetup.svg";
+import { IonCol, IonGrid, IonRow, IonContent, IonCard } from "@ionic/react";
+
+import SettingsCircle from "./Icons/SettingsCircle";
+import Flash from "./Icons/flash";
+import Detail from "./Icons/Detail";
 
 const EventType: React.FC = ({}) => {
   return (
@@ -18,7 +12,7 @@ const EventType: React.FC = ({}) => {
         <IonGrid className="ion-no-margin ion-no-padding action-line">
           <IonRow>
             <IonCol className="actions-head ion-padding-bottom">
-              <IonImg className="action-icon" src={settingsIcon} />
+              <SettingsCircle />
               <h3 className="ion-text-center action-title">Create Event</h3>
               <p className="ion-text-center action-subtitle">
                 Select quick or detailed event setup
@@ -28,7 +22,7 @@ const EventType: React.FC = ({}) => {
           <IonRow className="ion-padding-top">
             <IonCol className="ion-text-center ">
               <IonCard className="ion-padding event-type">
-                <IonImg className="type-icon" src={detailSetupIcon} />
+                <Detail />
                 <h2>Detailed setup</h2>
                 <p>
                   (Create a fully customized event with complete control over
@@ -38,7 +32,7 @@ const EventType: React.FC = ({}) => {
             </IonCol>
             <IonCol className="ion-text-center">
               <IonCard className="ion-padding event-type">
-                <IonImg className="type-icon" src={quickSetupIcon} />
+                <Flash />
                 <h2>Quick setup</h2>
                 <p>(Set up your event instantly with simple, fast options.)</p>
               </IonCard>
