609898fc9e3ed009d412eec4fdb63ccd81c4395e
syam <syamalj@innvectra.com>
2024-11-19 21:12:07 +0530

    Event list page updated

--- src/components/EventList/EventListPresenter.tsx
+++ src/components/EventList/EventListPresenter.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./EventListPresenter.scss";
-import { IonPage, IonGrid, IonContent, IonCol, IonRow } from "@ionic/react";
+import {
+  IonPage,
+  IonGrid,
+  IonContent,
+  IonCol,
+  IonRow,
+  IonFooter,
+  IonButton,
+  IonModal,
+} from "@ionic/react";
 import { EventListProps } from "@goflock/types/src/index";
 import EventItem from "../Common/Events/EventItem";
 import Header from "../Header/Header";
+import NoEvent from "../Common/Events/NoEvent";
+import EventType from "../Common/Events/EventType";
 
 const EventListPresenter: React.FC<EventListProps> = ({
   // profile,
   events,
   openEvent,
 }) => {
+  const modal = useRef<HTMLIonModalElement>(null);
   return (
     <IonPage>
       <Header
@@ -23,7 +35,7 @@ const EventListPresenter: React.FC<EventListProps> = ({
         {/* <IonLabel className="event-name">{profile.prefName}'s Events</IonLabel> */}
         <IonGrid className="events_sec ion-no-padding ion-no-margin">
           {events.length === 0 ? (
-            <p>No events to display.</p>
+            <NoEvent />
           ) : (
             <IonRow className="event-row">
               {events.map((event) => (
@@ -40,6 +52,26 @@ const EventListPresenter: React.FC<EventListProps> = ({
           )}
         </IonGrid>
       </IonContent>
+      <IonFooter className="stickyFooter">
+        <IonButton
+          color="primary"
+          shape="round"
+          size="large"
+          id="open-modal"
+          expand="block"
+        >
+          Create new event
+        </IonButton>
+      </IonFooter>
+      <IonModal
+        ref={modal}
+        trigger="open-modal"
+        initialBreakpoint={0.6}
+        breakpoints={[0, 0.6, 0.75]}
+        className="action-moadal ion-padding-top"
+      >
+        <EventType />
+      </IonModal>
     </IonPage>
   );
 };
