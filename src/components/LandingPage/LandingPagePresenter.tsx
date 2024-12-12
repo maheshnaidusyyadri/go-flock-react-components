2c5b62d55a2660a2a5ec8f44c29cf9184e1943bd
DileepkumarMypaty <dileepm@innvectra.com>
2024-12-12 19:41:48 +0530

    Creating the Landing page presenter

---
+++ src/components/LandingPage/LandingPagePresenter.tsx
@@ -0,0 +1,11 @@
+import { IonContent, IonPage, IonText } from "@ionic/react";
+const LandingPagePresenter: React.FC = ({}) => {
+  return (
+    <IonPage>
+      <IonContent>
+        <IonText>Landing Page Component</IonText>
+      </IonContent>
+    </IonPage>
+  );
+};
+export default LandingPagePresenter;
