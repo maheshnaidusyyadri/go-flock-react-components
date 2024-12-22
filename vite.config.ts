34ef5a4a07f0633c47fddb3e2e1d7ffef204049e
Gouthu <gouthu@MacBook-Pro.local>
2024-12-21 21:54:16 -0800

    Build optimization to remove react dom development.js to not load

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       formats: ["cjs", "es"],
     },
     rollupOptions: {
-      external: [...Object.keys([])],
+      external: ["react", "react-dom"],
     },
     sourcemap: true,
     emptyOutDir: true,
